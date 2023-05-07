67b04c8e364926e652c3c45220d8201a2edbeecd
Alejandro <mabtzr@gmail.com>
2023-05-07 12:53:09 -0500

    Used data notation or attributes for specific configurations and  restrictions

--- Models/ToDo.cs
+++ Models/ToDo.cs
@@ -1,15 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.Models;
 
 public class ToDo
 {
+    [Key]
     public Guid TaskId {get;set;}
+
+    [ForeignKey("CategoryId")]
     public Guid CategoryId {get; set;}
+
+    [Required]
+    [MaxLength(200)]
     public string Title {get; set;}
+
     public string Description {get; set;}
 
     public PriorityEnum Priority {get; set; }
     DateTime CreatedTime {get; set;}
     public virtual Category Category {get; set;}
+
+    [NotMapped]
+    public string Summary {get; set;}
 
 }
 
