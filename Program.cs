1d8bc378e404a685056138b6619d50abf04a82d7
Alejandro <mabtzr@gmail.com>
2023-05-08 10:29:47 -0500

    connected ef to sql server successfully

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TaskContext>(p => p.UseInMemoryDatabase("TaskDb"));
+builder.Services.AddSqlServer<TaskContext>("Data Source=Alejandro; Initial Catalog=tododatabase; user id=sa;password=12345678; TrustServerCertificate=true");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async([FromServices] TaskContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
