864e5f82f340d78f5681b8269bc70d455480333c
Alejandro <mabtzr@gmail.com>
2023-05-21 17:05:06 -0500

    rollback to commit when we start create using fluent api

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,14 +5,13 @@ namespace projectef.Models;
 
 public class Category
 {
-    [Key]
+    //[Key]
     public Guid CategoryId {get; set;}
 
-    [Required]
-    [MaxLength(150)]
+    //[Required]
+    //[MaxLength(150)]
     public string Name {get; set;}
     public string Description {get; set;}
-
     public virtual ICollection<ToDo> Tasks {get; set;}
 }
 
--- TaskContext.cs
+++ TaskContext.cs
@@ -9,4 +9,14 @@ public class TaskContext: DbContext
     public DbSet<Category> Categories {get; set; }
     public DbSet<ToDo> Task {get; set;}
     public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(cat => {
+            cat.ToTable("Category");
+            cat.HasKey(p => p.CategoryId);
+            cat.Property(p => p.Name).IsRequired().HasMaxLength(150);
+            cat.Property(p => p.Description);
+        });
+    }
 } 
